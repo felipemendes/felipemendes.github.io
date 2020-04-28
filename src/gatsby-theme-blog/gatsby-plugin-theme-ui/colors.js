124d1642e82e76c210571b6165a393280aca4b0f
Felipe Mendes <felipemendes@ioasys.com.br>
2020-04-27 21:45:26 -0300

    Update

--- src/gatsby-theme-blog/gatsby-plugin-theme-ui/colors.js
+++ src/gatsby-theme-blog/gatsby-plugin-theme-ui/colors.js
@@ -1,19 +1,19 @@
 import merge from "deepmerge"
 import defaultThemeColors from "gatsby-theme-blog/src/gatsby-plugin-theme-ui/colors"
 
-const orange = `#d73b24`
+const blue = `#051627`
 const lightGray = `#ddd`
 const darkBackground = `#222`
 
 export default merge(defaultThemeColors, {
   text: darkBackground,
-  primary: orange,
+  primary: blue,
   heading: darkBackground,
   modes: {
     dark: {
       background: darkBackground,
-      primary: lightBlue,
-      highlight: lightBlue,
+      primary: lightGray,
+      highlight: lightGray,
     },
   },
 })
