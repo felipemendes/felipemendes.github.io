2589fedc917f219ef7a44da38ac5bd636446ab7d
Felipe <felipemendes@me.com>
2020-04-29 17:26:13 -0300

    Update

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -3,6 +3,6 @@ import { Styled } from "theme-ui"
 
 export default () => (
   <Fragment>
-    Blog sobre desenvolvimento Swift. <Styled.a href="https://github.com/felipemendes">GitHub</Styled.a> e <Styled.a href="https://www.linkedin.com/in/felipermendes/">LinkedIn</Styled.a>.
+    Blog sobre desenvolvimento Swift. \n<Styled.a href="https://github.com/felipemendes">GitHub</Styled.a> e <Styled.a href="https://www.linkedin.com/in/felipermendes/">LinkedIn</Styled.a>.
   </Fragment>
 )
